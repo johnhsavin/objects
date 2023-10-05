aee1b04162f87a15b80b11ec79a5a2de4cb62fb5
John Savin <johnsavin@proton.me>
2023-10-05 16:57:25 -0400

    my first object

---
+++ index.js
@@ -0,0 +1,15 @@
+const person = {  //Declare new object
+    firstName: "Todd",
+    age: 28
+}
+
+console.log(person.firstName)   //print person's name
+
+// add one to the person's age
+person.age++
+
+console.log(person.age)
+
+person.lastName = "Albert"
+console.log(person.lastName)
+console.log(person)
